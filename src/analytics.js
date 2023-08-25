590cb0595103679249816bf68b76d16f54bb6b00
Sourabh Niyogi <sourabh@colorfulnotion.com>
2023-08-25 18:11:17 +0000

    analytics generatelogs test; uniswapv2 notes

---
+++ src/analytics.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+const CaladanDB = require("./caladanDB");
+const fs = require("fs");
+const jsonc = require("jsonc");
+const organization = "CaladanDAO";
+const projectID = "awesome-web3";
+const baseDir = `/root/go/src/github.com/${organization}/${projectID}`
+
+module.exports = class Analytics extends CaladanDB {
+    constructor() {
+        super("manager")
+    }
+
+    async parse(fn = "../defi/uniswap/config.jsonld") {
+        const datac = fs.readFileSync(fn, {
+            encoding: 'utf8',
+            flag: 'r'
+        });
+        let projects = jsonc.parse(datac);
+        const jsonld = require('jsonld');
+        const flattened = await jsonld.flatten(projects);
+	console.log(JSON.stringify(flattened, null, 4));
+    }
+    async generatelogs() {
+	// bigtable test - read wrapped ethereum record from accountrealtime
+	try {
+	    let rowKeys = ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"];
+            const [rows] = await this.btAccountRealtime.getRows({
+		keys: rowKeys,
+		filter: [{
+                    family: ["metadata"],
+                    column: {
+			cellLimit: 1
+		    }
+		}]
+            });
+	    console.log("bigtable test: ", JSON.stringify(rows[0].data, null, 4));
+	} catch (err) {
+	    console.log("bigtable error", err);
+	}
+
+	// mysql test - read chain latest block
+	try {
+	    let sql = "select chainID, id, blocksCovered from chain where crawling = 1";
+	    let chains = await this.poolREADONLY.query(sql)
+	    if ( chains.length > 0 ) {
+		console.log("mysql test -- # chains: ", chains.length);
+		for ( const c of chains ) {
+		    console.log(JSON.stringify(c));
+		}
+	    } else {
+		console.log("mysql test FAIL", sql);
+	    }
+	} catch (err) {
+	    console.log("mysql error", err);
+	}
+	
+	// bigquery test
+	let blockNumber = null
+	let blockTS = null
+	try {
+            let query = `SELECT _TABLE_SUFFIX as chainID,  count(\`hash\`) numTransactions, UNIX_SECONDS(max(block_timestamp)) blockTS, max(block_number) blockNumber
+ FROM \`${projectID}.caladan_evm.transactions*\` WHERE block_timestamp >= DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 HOUR) 
+ group by chainID LIMIT 1000;`
+            let recs = await this.execute_bqJob(query);
+	    if ( recs.length > 0 ) {
+		console.log("bigquery test -- # chains: ", recs.length);
+		for ( const c of recs ) {
+		    console.log(JSON.stringify(c));
+		    if ( c.chainID == 1 ) {
+			blockNumber = c.blockNumber
+			blockTS = c.blockTS
+		    }
+		}
+	    } else {
+		console.log("bigquery test FAIL", query);
+	    }
+	} catch (err) {
+	    console.log("bigquery error", err);
+	}
+	
+	// google storage test - read latest block of ethereum using above
+	try {
+	    const date = new Date(blockTS * 1000); // Convert Unix timestamp to milliseconds
+	    const year = date.getFullYear();
+	    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+	    const day = String(date.getDate()).padStart(2, '0');
+	    let logDT = `${year}/${month}/${day}`;
+	    let filePath = `1/${logDT}/${blockNumber}.json.gz`
+	    const {
+		Storage
+	    } = require('@google-cloud/storage');
+
+	    
+	    let storage = new Storage();
+	    const bucket = storage.bucket("caladan_evm");
+	    const file = bucket.file(filePath);
+            const buffer = await file.download();
+            const r = JSON.parse(buffer[0]); // block, receipts, evm
+	    console.log("gs storage blockNumber", blockNumber, "ts=", blockTS, "blockHash=", r.block.hash, "#Txs=", r.block.transactions.length,  "#Receipts=", r.receipts.length, " #Traces=", r.traces.length);
+	} catch (err) {
+	     console.log("gs storage error", err);
+	}
+    }
+
+}
---
+++ src/caladanDB.js
@@ -0,0 +1,185 @@
+// Copyright 2023 
+// This file is part of Caladan DAO Analytics
+
+const ini = require('node-ini');
+
+const {
+    Bigtable
+} = require("@google-cloud/bigtable");
+const {
+    BigQuery
+} = require('@google-cloud/bigquery');
+const {
+    Storage
+} = require('@google-cloud/storage');
+
+const mysql = require("mysql2");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const path = require('path');
+const fs = require('fs');
+const os = require("os");
+
+module.exports = class CaladanDB {
+    GC_PROJECT = "";
+    GC_BIGTABLE_INSTANCE = "";
+    GC_BIGTABLE_CLUSTER = "";
+    GC_STORAGE_BUCKET = "";
+
+    CALADAN_EMAIL_USER = "";
+    CALADAN_EMAIL_PASSWORD = "";
+
+    BQ_KEY = null;
+
+    bigQuery = null;
+    googleStorage = null;
+    bqTablesCallsEvents = null;
+
+    showMemoryUsage(ctx = "") {
+        const used = process.memoryUsage().heapUsed / 1024 / 1024;
+        console.log(`${ctx} USED: [${used}MB]`)
+    }
+
+    constructor(serviceName = "analytics") {
+        this.hostname = os.hostname();
+        this.version = `1.0.0` // we will update this manually
+
+        // 1. ready db config for WRITABLE mysql pool [always in US presently] using env variable POLKAHOLIC_DB
+        let dbconfigFilename = (process.env.CALADAN_DB != undefined) ? process.env.CALADAN_DB : '/root/.mysql/.db00.cnf';
+        try {
+            let dbconfig = ini.parseSync(dbconfigFilename);
+            if (dbconfig.email != undefined) {
+                this.POLKAHOLIC_EMAIL_USER = dbconfig.email.email;
+                this.POLKAHOLIC_EMAIL_PASSWORD = dbconfig.email.password;
+            }
+            if (dbconfig.gc != undefined) {
+                this.GC_PROJECT = dbconfig.gc.projectName;
+                this.GC_BIGTABLE_INSTANCE = dbconfig.gc.bigtableInstance;
+                this.GC_BIGTABLE_CLUSTER = dbconfig.gc.bigtableCluster;
+                this.GC_STORAGE_BUCKET = dbconfig.gc.storageBucket;
+            }
+
+            if (dbconfig.bq != undefined) {
+                if (dbconfig.bq.substrateetlKey) {
+                    this.BQ_KEY = dbconfig.bq.substrateetlKey;
+                }
+            }
+
+            if (dbconfig.externalapis != undefined) {
+                if (dbconfig.externalapis) {
+                    this.EXTERNAL_APIKEYS = dbconfig.externalapis;
+                }
+            }
+
+            this.pool = mysql.createPool(this.convert_dbconfig(dbconfig.client));
+            // Ping WRITABLE database to check for common exception errors.
+            this.pool.getConnection((err, connection) => {
+                if (err) {
+                    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                        console.error('Database connection was closed.')
+                    }
+                    if (err.code === 'ER_CON_COUNT_ERROR') {
+                        console.error('Database has too many connections.')
+                    }
+                    if (err.code === 'ECONNREFUSED') {
+                        console.error('Database connection was refused.')
+                    }
+                }
+
+                if (connection) {
+                    this.connection = connection;
+                    connection.release()
+                }
+                return
+            })
+            // Promisify for Node.js async/await.
+            this.pool.query = util.promisify(this.pool.query).bind(this.pool)
+            this.pool.end = util.promisify(this.pool.end).bind(this.pool)
+        } catch (err) {
+            console.log(err);
+            this.logger.fatal({
+                "op": "dbconfig",
+                err
+            });
+        }
+
+        // 2. ready db config for READONLY mysql pool [default US replica but could be EU/AS replica] using env variable CALADAN_DB_REPLICA
+        let dbconfigReplicaFilename = (process.env.CALADAN_DB_REPLICA != undefined) ? process.env.CALADAN_DB_REPLICA : '/root/.mysql/.db00-us-indexer.cnf';
+        try {
+            let dbconfigREADONLY = ini.parseSync(dbconfigReplicaFilename);
+            this.poolREADONLY = mysql.createPool(this.convert_dbconfig(dbconfigREADONLY.client));
+            // Ping READONLY database to check for common exception errors.
+            this.poolREADONLY.getConnection((err, connection) => {
+                if (err) {
+                    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                        console.error('Database connection was closed.')
+                    }
+                    if (err.code === 'ER_CON_COUNT_ERROR') {
+                        console.error('Database has too many connections.')
+                    }
+                    if (err.code === 'ECONNREFUSED') {
+                        console.error('Database connection was refused.')
+                    }
+                }
+
+                if (connection) {
+                    this.connectionREADONLY = connection;
+                    connection.release()
+                }
+                return
+            })
+            // Promisify for Node.js async/await.
+            this.poolREADONLY.query = util.promisify(this.poolREADONLY.query).bind(this.poolREADONLY)
+            this.poolREADONLY.end = util.promisify(this.poolREADONLY.end).bind(this.poolREADONLY)
+        } catch (err) {
+            console.log(err);
+        }
+        const bigtable = new Bigtable({
+            projectId: this.GC_PROJECT
+        });
+        const instanceName = this.GC_BIGTABLE_INSTANCE;
+        this.instance = bigtable.instance(instanceName);
+        this.btAccountRealtime = this.instance.table("accountrealtime");
+    }
+
+    convert_dbconfig(c) {
+        return {
+            host: c.host,
+            user: c.user,
+            password: c.password,
+            database: c.database,
+            charset: c['default-character-set']
+        };
+    }
+
+    get_big_query() {
+        if (this.bigQuery) return this.bigQuery;
+        this.bigQuery = new BigQuery({
+            projectId: this.GC_PROJECT,
+            keyFilename: this.BQ_KEY
+        })
+        return this.bigQuery;
+    }
+
+    async execute_bqJob(sqlQuery, targetBQLocation = null) {
+        // run bigquery job with suitable credentials
+        const bigqueryClient = this.get_big_query();
+        const options = {
+            query: sqlQuery,
+            location: (targetBQLocation) ? targetBQLocation : this.defaultBQLocation,
+        };
+
+        try {
+            const response = await bigqueryClient.createQueryJob(options);
+            const job = response[0];
+            const [rows] = await job.getQueryResults();
+            return rows;
+        } catch (err) {
+            console.log(err);
+            throw new Error(`An error has occurred.`, sqlQuery);
+        }
+        return [];
+    }
+
+    
+}
